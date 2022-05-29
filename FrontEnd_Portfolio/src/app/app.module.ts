259071eeab5936c166c5b67cd2db5dc1515c8f3b
Luky496 <lucasstella496@gmail.com>
2022-05-29 15:34:03 -0300

    staged

--- FrontEnd_Portfolio/src/app/app.module.ts
+++ FrontEnd_Portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAppComponent } from './components/header/logo-app/logo-app.component';
+import { SocialComponent } from './components/header/social/social.component';
+import { BannerComponent } from './components/header/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAppComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule,
